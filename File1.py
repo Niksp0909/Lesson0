e51c29dc54a18229bd0ce84546e40671d8f531a5
Niksp0909 <niksp09@mail.ru>
2024-05-21 15:56:03 +0300

    Add files via upload

---
+++ File1.py
@@ -0,0 +1,31 @@
+# Длина слова
+a = 'пример'
+a = len(a)
+print(a)
+
+# Суммы и разности
+first = 5
+second = 6
+summa = first + second
+diff = first - second
+print(summa)
+print(diff)
+
+# Среднее арифметическое
+first = 1
+second = 2
+third = 3
+mean = (first+second+third)/3
+print(mean)
+
+# Простые строчки
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+
+# Сложная формула
+a = 3
+b = 5
+c = 2
+f = (((a * b) + (a * c))**3)/2
+print(f)
