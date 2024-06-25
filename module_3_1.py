683fbc9062bb5144ab75285c1e83c1cbad4e1739
Niksp0909 <niksp09@mail.ru>
2024-06-25 15:18:41 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,24 @@
+calls = 0
+
+def count_calls(func):
+    def wrapper(*args, **kwargs):
+        global calls
+        calls += 1
+        return func(*args, **kwargs)
+    return wrapper
+
+@count_calls
+def string_info(string):
+    return (len(string), string.upper(), string.lower())
+
+@count_calls
+def is_contains(string, list_to_search):
+    string = string.lower()
+    list_to_search = [item.lower() for item in list_to_search]
+    return string in list_to_search
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycle', 'cyclic']))
+print(calls)
